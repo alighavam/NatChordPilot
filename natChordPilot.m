91a1ba1465e9d44b2c762f550c6104318d81248b
Ali Ghavampour <alighavam79@gmail.com>
2023-08-29 16:44:26 -0400

    initial commit

---
+++ natChordPilot.m
@@ -0,0 +1,21 @@
+% Ali Ghavampour 2023 - Diedrichsen & Pruszynski lab
+clear;
+clc;
+close all;
+
+% Adding required tools and packages: 
+addpath(genpath('/Users/aghavampour/Desktop/matlab/dataframe-2016.1'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
