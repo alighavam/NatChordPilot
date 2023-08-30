f68ef0116ab5894e446670c4d30c5232c96036fe
Ali Ghavampour <alighavam79@gmail.com>
2023-08-29 21:53:58 -0400

    Update natChordPilot.m

--- natChordPilot.m
+++ natChordPilot.m
@@ -3,9 +3,42 @@ clear;
 clc;
 close all;
 
-% Adding required tools and packages: 
-addpath(genpath('/Users/aghavampour/Desktop/matlab/dataframe-2016.1'))
+% setting paths:
+usr_path = userpath;
+usr_path = usr_path(1:end-17);
 
+
+% Adding required tools and packages:
+addpath(genpath(fullfile(usr_path,'Desktop/matlab/dataframe-2016.1')),'-begin')
+
+% Loading data:
+subj_name = 'subj01';
+dat_file_name = ['data/',subj_name,'/','efc1_',...
+                 num2str(str2num(subj_name(end-1:end))),'.dat'];
+D = dload(dat_file_name);
+ANA = [];
+
+trials = 1:length(D.BN);
+
+oldBlock = -1;
+for i = trials
+    if (oldBlock ~= D.BN(i))
+        % load mov file for the block
+        fprintf("Loading .mov file %d.\n",D.BN(i))
+        mov = movload(['data/' subj_name '/' 'efc1_' num2str(str2num(subj_name(end-1:end))) '_' num2str(D.BN(i),'%02d') '.mov']);
+        oldBlock = D.BN(i);
+    end
+    fprintf('Block: %d , Trial: %d\n',D.BN(i),D.TN(i));
+
+    row = getrow(D,i);
+    C = [];
+    if (row.trialPoint==0)
+        row.RT = 10000;   % correction for execution error trials
+    end
+    C = addstruct(C,row,'row','force');
+    C.mov(D.TN(i)) = mov(D.TN(i));
+    ANA=addstruct(ANA,C,'row','force');
+end
 
 
 
